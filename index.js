1d5e5114608ade0e07213c85da7a4626b1e2bc74
Aymeric987 <aymeric.guny@adeo.com>
2025-05-28 14:13:03 +0200

    feat: wip

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ async function run() {
         core.info('Search for missing package-lock.json files');
         const globber = await glob.create(patterns.join('\n'));
         const files = await globber.glob();
+        let analysedPackageJsonCount = files.length
+        let missingPackageLockJsonCount = 0
         files.forEach(file => {
             try {
                 fs.accessSync(path.join(path.dirname(file), 'package-lock.json'));
             } catch (err) {
+                missingPackageLockJsonCount++;
                 core.warning('Consider to generate it and commit it', {
                     file: file,
                     title: 'Missing package-lock.json'
                 })
             }
         });
+        let message = '';
+        if (analysedPackageJsonCount === 0) {
+            message = 'No package.json found';
+        } else if (missingPackageLockJsonCount === 0) {
+            message = `No missing package-lock.json files based on ${analysedPackageJsonCount} package.json files analysed`;
+        } else {
+            message = `Missing ${missingPackageLockJsonCount} package-lock.json files based on ${analysedPackageJsonCount} package.json files analysed`;
+        }
+        core.summary.addHeading("Package.json analysis");
+        core.summary.addRaw(message);
     } catch (error) {
         core.setFailed(error.message);
     }
