e7904c275a9ba0d68e75d3207da5aabc93c5fcc8
Aymeric987 <aymeric.guny@adeo.com>
2025-05-28 13:38:52 +0200

    feat: wip

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const glob = require('@actions/glob');
 const core = require('@actions/core');
+
+const pattern = '*/package-lock.json'
 
 async function run() {
     try {
         core.info('Search for missing package-lock.json files');
+        const globber = await glob.create(pattern);
+        const files = await globber.glob();
+        files.forEach(file => {
+            core.info(file);
+        })
     }
     catch (error) {
         core.setFailed(error.message);
